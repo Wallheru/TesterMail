8bbde162b518ceeed0ff841da67ab3d8a67d298f
Wallheru <davidek.bezdeka@gmail.com>
2026-04-16 20:01:33 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,28 +2,26 @@ require('dotenv').config();
 
 const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 
-const CLIENT_ID = "1494367658794029206"; // your app id
+const CLIENT_ID = "1494367658794029206";
 
 const commands = [
 
-  // ===== /send =====
   new SlashCommandBuilder()
     .setName('send')
-    .setDescription('Send a DM to a user')
+    .setDescription('Send a DM')
     .addStringOption(option =>
       option.setName('userid')
         .setDescription('User ID')
         .setRequired(true))
     .addStringOption(option =>
       option.setName('message')
-        .setDescription('Message to send')
+        .setDescription('Message')
         .setRequired(true))
     .addAttachmentOption(option =>
       option.setName('file')
-        .setDescription('Optional image/file')
+        .setDescription('Image/file')
         .setRequired(false)),
 
-  // ===== /reply =====
   new SlashCommandBuilder()
     .setName('reply')
     .setDescription('Reply to a user')
@@ -37,7 +35,7 @@ const commands = [
         .setRequired(true))
     .addAttachmentOption(option =>
       option.setName('file')
-        .setDescription('Optional image/file')
+        .setDescription('Image/file')
         .setRequired(false))
 
 ].map(cmd => cmd.toJSON());
@@ -46,15 +44,15 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log("⏳ Registering slash commands...");
+    console.log("Registering commands...");
 
     await rest.put(
       Routes.applicationCommands(CLIENT_ID),
       { body: commands }
     );
 
-    console.log("✅ Slash commands registered!");
-  } catch (error) {
-    console.error("❌ Error:", error);
+    console.log("✅ Commands registered!");
+  } catch (err) {
+    console.error(err);
   }
 })();
